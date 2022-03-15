5ebb10c2b8be1422a6a7d3ed36ff5f4e936d190b
Caroline Fairhurst <cfairhurst@protonmail.com>
2022-03-15 12:10:56 -0400

    remote access

--- config/config.js
+++ config/config.js
@@ -46,6 +46,21 @@ let config = {
 
 	modules: [
 		{
+			module: "MMM-Remote-Control",
+			// uncomment the following line to show the URL of the remote control on the mirror
+			// position: 'bottom_left',
+			// you can hide this module afterwards from the remote control itself
+			config: {
+				customCommand: {}, // Optional, See "Using Custom Commands" below
+				showModuleApiMenu: true, // Optional, Enable the Module Controls menu
+				secureEndpoints: true // Optional, See API/README.md
+				// uncomment any of the lines below if you're gonna use it
+				// customMenu: "custom_menu.json", // Optional, See "Custom Menu Items" below
+				// apiKey: "", // Optional, See API/README.md for details
+				// classes: {} // Optional, See "Custom Classes" below
+			}
+		},
+		{
 			module: "MMM-MagicMover",
 			config: {
 				updateInterval: 60 * 1000,
